e889f809a3a056ba596e602a134b54f557a943c5
BBK-PiJ-2016-62 <chunghoichiu@yahoo.com>
2020-07-20 16:59:14 +0100

    added an r file: keras and structured data

---
+++ preparationWorks/youTubeDeepLearningWithKerasNTensorFlowInR/deeplearning_gapsFilled.R
@@ -0,0 +1,91 @@
+# Install packages
+library(keras)
+#install_keras() #no need here
+
+# Use here. Important here
+use_condaenv("r_reticulate")
+
+# Read data
+data <- read.csv(file.choose(), header = T)
+str(data)
+
+# Change to matrix
+data <- as.matrix(data)
+dimnames(data) <- NULL
+
+# Normalize
+data[, 1:21] <- normalize(data[, 1:21])
+data[,22] <- as.numeric(data[,22]) -1
+summary(data)
+
+# Data partition
+set.seed(1234)
+ind <- sample(2, nrow(data), replace = T, prob = c(0.7, 0.3))
+training <- data[ind==1, 1:21]
+test <- data[ind==2, 1:21]
+trainingtarget <- data[ind==1, 22]
+testtarget <- data[ind==2, 22]
+
+# One Hot Encoding
+trainLabels <- to_categorical(trainingtarget)
+testLabels <- to_categorical(testtarget)
+print(testLabels)
+
+# Create sequential model
+model <- keras_model_sequential()
+model %>% #one hidden layer, units = 8 (21 input columns, 3 categories)
+         layer_dense(units=8, activation = 'relu', input_shape = c(21)) %>%
+         layer_dense(units = 3, activation = 'softmax')
+summary(model)
+
+## 2nd: one hidden layer, units = 50
+model <- keras_model_sequential()
+model %>%
+         layer_dense(units=50, activation = 'relu', input_shape = c(21)) %>%
+         layer_dense(units = 3, activation = 'softmax')
+		 
+## 3rd: two hidden layers, 1st units = 50, 2nd units = 8
+model <- keras_model_sequential()
+model %>%
+         layer_dense(units=50, activation = 'relu', input_shape = c(21)) %>%
+         layer_dense(units=8, activation = 'relu') %>%
+		 layer_dense(units = 3, activation = 'softmax')	
+
+## try different number of layers and units
+	model <- keras_model_sequential()
+model %>%
+         layer_dense(units=84, activation = 'relu', input_shape = c(21)) %>%
+         layer_dense(units=42, activation = 'relu') %>%
+		 layer_dense(units=21, activation = 'relu') %>%
+		 layer_dense(units = 3, activation = 'softmax') 
+
+# Compile
+model %>%
+         compile(loss = 'categorical_crossentropy',
+                 optimizer = 'adam',
+                 metrics = 'accuracy')
+
+# Fit model
+history <- model %>%
+         fit(training,
+             trainLabels,
+             epoch = 200,
+             batch_size = 32,
+             validation_split = 0.2)
+plot(history)
+
+# Evaluate model with test data
+model1 <- model %>%
+         evaluate(test, testLabels)
+
+# Prediction & confusion matrix - test data
+prob <- model %>%
+         predict_proba(test)
+
+pred <- model %>%
+         predict_classes(test)
+table <- table(Predicted = pred, Actual = testtarget)
+
+cbind(prob, pred, testtarget)
+
+# Fine-tune model
